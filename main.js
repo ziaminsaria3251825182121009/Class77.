d7db6e04553dd0cf89263ac51a668c1525a7336c
ziaminsaria3251825182121009 <118575122+ziaminsaria3251825182121009@users.noreply.github.com>
2023-01-05 17:04:47 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("My first console project")
+
+ var first_name =  "Zia"
+ var last_name =  "Minsaria"
+
+ var full_name = first-name.concat(last_name);
+ console.log(full_name);
+ var a = 10;
+ var b = 5;
+
+ var add = a+b ;
+ console.log(add);
+
+ var sub = a-b ;
+ console.log ("sub");
+
+ var multiple = a * b ;
+ console.log(multiple);
+
+ var divide = a/b ;
+ console.log(divide);
+
+  
+ 
